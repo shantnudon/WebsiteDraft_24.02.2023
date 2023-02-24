4353fb105e5e262dce726efb3339820d6eda5370
Shantnu03 <shantnu04@gmail.com>
2023-02-24 20:04:28 +0530

    Update _navbar.php

--- _navbar.php
+++ _navbar.php
@@ -1,12 +1,9 @@
-<?php
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-  $loggedin = true;
-} else {
-  $loggedin = false;
-}
-echo '
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<link
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+  rel="stylesheet"
+  integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+  crossorigin="anonymous"
+/>
 <script
   src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
@@ -37,33 +34,24 @@ integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6
             >Home_NavBar</a
           >
         </li>
-
-        ';
-if (!$loggedin) {
-  echo '
         <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="/crud/login.php"
-        >Log In</a
-        >
+          <a class="nav-link active" aria-current="page" href="/crud/login.php"
+            >Log In</a
+          >
         </li>
         <li class="nav-item">
-        <a
-        class="nav-link active"
-        aria-current="page"
-        href="/crud/register.php"
-        >Register</a
-        >
+          <a
+            class="nav-link active"
+            aria-current="page"
+            href="/crud/register.php"
+            >Register</a
+          >
         </li>
-        ';
-}
-if ($loggedin) {
-  echo '
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/crud/logout.php">logout</a>
+          <a class="nav-link active" aria-current="page" href="/crud/logout.php"
+            >logout</a
+          >
         </li>
-        ';
-}
-echo '
       </ul>
       <form class="d-flex" role="search">
         <input
@@ -77,5 +65,4 @@ echo '
     </div>
   </div>
 </nav>
-';
-?>
+'; ?>
